32c2433c23650a4153539c940ead06877d7b87a3
eliasyacoub8 <eliasyacoub74@gmail.com>
2025-12-15 01:17:01 +0200

    fourth commit

--- src/components/SummaryCards.tsx
+++ src/components/SummaryCards.tsx
@@ -6,23 +6,23 @@ export default function SummaryCards() {
   const balance = useTransactionStore((state) => state.getBalance());
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 w-full max-w-4xl mt-10 mr-2">
+    <div className="grid grid-cols-3 gap-6 w-full mt-10 ml-2">
       {/* Income Card */}
-      <div className="bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
+      <div className="w-full bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
         <h3 className="text-lg font-semibold text-gray-600 h-10 flex items-center justify-center whitespace-nowrap">
           Total Income
         </h3>
         <p className="text-3xl font-bold text-green-600 mt-2">${income}</p>
       </div>
       {/* Expense Card */}
-      <div className="bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
+      <div className="w-full bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
         <h3 className="text-lg font-semibold text-gray-600 h-10 flex items-center justify-center whitespace-nowrap">
           Total Expenses
         </h3>
         <p className="text-3xl font-bold text-red-600 mt-2">${expenses}</p>
       </div>
       {/* Balance Card */}
-      <div className="bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
+      <div className="w-full bg-white p-6 shadow-md rounded-xl border border-gray-100 flex flex-col items-center text-center">
         <h3 className="text-lg font-semibold text-gray-600 h-10 flex items-center justify-center whitespace-nowrap">
           Total Balance
         </h3>
--- src/pages/TransactionPage.tsx
+++ src/pages/TransactionPage.tsx
@@ -4,10 +4,10 @@ import SummaryCards from "../components/SummaryCards";
 export default function TransactionPage() {
   return (
     <div className="flex gap-10 w-full px-10 py-6">
-      <div className="w-1/2">
+      <div className="w-1/2 mt-10">
         <AddTransactionPanel />
       </div>
-      <div className="w-1/2 space-y-6">
+      <div className="w-1/2">
         <SummaryCards />
         <TransactionTable />
       </div>
