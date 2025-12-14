c8208d18eae1b0ee3f08be9abf6401cb6bedf1f6
eliasyacoub8 <eliasyacoub74@gmail.com>
2025-12-15 01:24:26 +0200

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import TransactionPage from "./pages/TransactionPage";
 import Sidebar from "./components/Sidebar";
+import LoginPage from "./pages/LoginPage";
 
 export default function App() {
   const [page, setPage] = useState("dashboard");
   return (
     <div className="flex h-screen w-screen overflow-hidden bg-gray-200">
-      <Sidebar current={page} onChange={setPage} />
+      {page !== "signout" && <Sidebar current={page} onChange={setPage} />}
       <div className="flex-1 overflow-y-auto p-6">
         {page === "transactions" && <TransactionPage />}
+        {page === "signout" && <LoginPage />}
       </div>
     </div>
   );
